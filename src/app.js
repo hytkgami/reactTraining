22b4bb4a5ecc869d9092916978f70ed2702d41b4
hytkgami <gagagatrump@gmail.com>
2017-03-18 20:51:07 +0900

    feature fetch github repositories

--- src/app.js
+++ src/app.js
@@ -1,9 +1,40 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+const REQUEST_URL = 'https://api.github.com/users/hytkgami/repos'
+
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData()
+  }
+
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          data: responseData,
+        })
+      })
+  }
+
   render () {
-    return <p>Hello React!</p>;
+    return(
+        <ul>
+          {this.state.data.map((item) => {
+             return (
+                 <li key={item.id}>{item.name}</li>
+             )
+          })}
+        </ul>
+    )
   }
 }
 

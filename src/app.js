a59693131ffd54170977c4858b3c547320d2445c
hytkgami <gagagatrump@gmail.com>
2017-03-18 16:25:59 +0900

    add test codes

--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
-const sum = (a, b) => {
-  return a + b;
-};
+import React from 'react';
+import {render} from 'react-dom';
+
+class App extends React.Component {
+  render () {
+    return <p>Hello React!</p>;
+  }
+}
+
+render(<App/>, document.getElementById('app'));
